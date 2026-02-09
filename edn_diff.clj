af666fd362e280e9a65bf34804f35c0fbb37adb0
Phil Hofmann <phil@200ok.ch>
2026-02-09 11:05:36 +0100

    .

--- edn_diff.clj
+++ edn_diff.clj
@@ -2,17 +2,18 @@
 
 (ns edn-diff
   (:require [babashka.process :refer [shell]]
+            [clojure.data :as data]
             [clojure.edn :as edn]
             [clojure.string :as str]
+            [clojure.pprint :as pprint]
             [lambdaisland.deep-diff2 :as diff2]
-            [lambdaisland.deep-diff2.printer :as printer]
             [shell-smith.core :as smith]))
 
 (def usage "
 edn-diff
 
 Usage:
-  edn-diff [-aerb=<branch>] <edn-file>
+  edn-diff [-a|-r] [-e] [-b=<branch>] <edn-file>
   edn-diff -h
 
 Options:
@@ -23,7 +24,7 @@ Options:
   -h --help             Show help
 ")
 
-(def config (smith/config usage))
+(def config (smith/config usage :name "end_diff"))
 
 (defn read-edn-from-branch [branch file-path]
   (-> (shell {:out :string} "git" "show" (str branch ":" file-path))
@@ -31,12 +32,27 @@ Options:
       edn/read-string))
 
 (defn -main [& _args]
-  (let [{:keys [branch edn-file]} (smith/config usage)
+  (let [{:keys [branch edn-file added-only removed-only edn-output]} (smith/config usage)
         current-branch (str/trim (:out (shell {:out :string} "git" "branch" "--show-current")))
         current-data (read-edn-from-branch current-branch edn-file)
-        other-data (read-edn-from-branch branch edn-file)
-        diff-result (diff2/diff current-data other-data)]
-    (printer/print diff-result)))
+        other-data (read-edn-from-branch branch edn-file)]
+    (cond
+      added-only
+      (let [diff (first (data/diff current-data other-data))]
+        (if edn-output
+          (prn diff)
+          (pprint/pprint diff)))
+
+      removed-only
+      (let [diff (second (data/diff current-data other-data))]
+        (if edn-output
+          (prn diff)
+          (pprint/pprint diff)))
+
+      :else
+      (if edn-output
+        (prn (data/diff current-data other-data))
+        (diff2/pretty-print (diff2/diff current-data other-data))))))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
